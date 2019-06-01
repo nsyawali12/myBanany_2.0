5eb6c7a5fca01c05e053665eee7ed30d8a571c09
nsyawali12 <nsyawali12@gmail.com>
2019-06-01 14:35:16 +0700

    v24

--- banany_v23/app/src/main/java/com/example/banany_23/MainActivity.java
+++ banany_v23/app/src/main/java/com/example/banany_23/MainActivity.java
@@ -4,6 +4,25 @@ import android.app.Activity;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build.VERSION;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 /*
@@ -13,18 +32,107 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Toolbar myToolbar;
+    public Button ConnectButton;
+    public int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
+    AddServerActivity activityAddServer;
+    public GridView gridView;
+    FtpDatabaseHelper helper = new FtpDatabaseHelper(this);
+    ArrayList<String> serverList = new ArrayList();
+
     /*
     *
     * Method called at activity Creation
     * */
 
-    @Override
+    class C03221 implements OnClickListener{
+        C03221(){
+        }
+
+        public void OnClick(View v){
+            Intent intent = new Intent(MainActivity, ClientActivity.class);
+            MainActivity.this.helper.insertServer("BANANY 2.0", "192.168.4.1", 21, "pi", "banany", "/storage/emulated/0/", "/media/pi");
+            MainActivity.this.setExtras(intent, "BANANY");
+            MainActivity.this.startActivity(intent);
+        }
+    }
+
+    public static void deleteCache(Context context){
+        try {
+            deleteDir(context.getCacheDir());
+        } catch (Exception e){
+        }
+    }
+
+    public static boolean deleteDir(File dir){
+        if(dir != null && dir.isDirectory()){
+            String[] children = dir.list();
+            for (String file : children){
+                if (!deleteDir(new File(dir, file))){
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if (dir == null || !dir.isFile()){
+            return false;
+        } else {
+            return dir.delete();
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(C0322R.menu.main_action_bar, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() != C0322R.id.about){
+            return false;
+        }
+        Dialog dialog = new Dialog(this);
+        dialog.setTitle("About...");
+        dialog.setContentView(C0322R.layout.about_dialog);
+        dialog.show();
+        return true;
+    }
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView((int) C0322R.layout.activity_main2);
+        myToolbar = (Toolbar) findViewById(C0322R.id.mainToolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setIcon((int) C0322R.mipmap.ic_launcher);
+        getPermissions();
+        ((Button) findViewById(C0322R.id.ConnectButton)).setOnClickListener(new C03221());
     }
+
+    protected void onResume(){
+        super.onResume();
+        this.serverList = this.helper.getAllServers();
+        LinearLayout linearLayout1 = (LinearLayout) findViewById(C0322R.id.noitemsLinearLayout);
+        LinearLayout linearLayout2 = (LinearLayout) findViewById(C0322R.id.placeholderLinearLayout);
+        if (this.serverList.size() == 0){
+            linearLayout1.setVisibility(0);
+            linearLayout2.setVisibility(8);
+        } else {
+            linearLayout1.setVisibility(8);\
+            linearLayout2.setVisibility(0);
+        }
+        Iterator it = this.serverList.iterator();
+        while (it.hasNext()) {
+            String i = (String) it.next();
+            this.gridView.setAdapter(new CustomGridAdapter(this, this.serverList));
+        }
+    }
+
+    public void setExtras(Intent intent, String ServerName){
+        Cursor cursor = this.helper.getServerDetails(ServerName);
+        cursor.moveToFirst();
+        intent.putExtra()
+    }
+
+
 
 
 }
